2e91ff3bacfa73ff7e4bc338b9617c6820c7c17d
root <root@ip-172-31-13-44.us-east-2.compute.internal>
2018-08-03 06:35:40 -0400

    Adding docker-compose.yml file and creating a new mongo docker image

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
 //var db = mongojs('mongodb://brad:brad@ds047666.mlab.com:47666/mytasklist_brad', ['tasks']);
-var db = mongojs('mongodb://admin:123ali@ds257640.mlab.com:57640/creating-node-api', ['tasks']);
+//var db = mongojs('mongodb://admin:123ali@ds257640.mlab.com:57640/creating-node-api', ['tasks']);
+// We are adding Docker-compose.yaml file and will create a underlying layer of docker with mongo so no need to connect URL of MongoDB
+var db = mongojs('mongodb://mongo:27017', ['tasks']);
+
+
 
 // Get All Tasks
 router.get('/tasks', function(req, res, next){
